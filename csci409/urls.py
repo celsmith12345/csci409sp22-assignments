d323be5caa0d2955391258f0075da0415b374848
unknown <cel.smith12345@gmail.com>
2022-04-13 19:30:20 -0400

    Week 12 Assignment

--- csci409/urls.py
+++ csci409/urls.py
@@ -23,9 +23,11 @@ import tickets.views
 
 
 urlpatterns = [
+    path('', airports.views.index),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('airports', include('airports.urls')),
-    path('flights/search', include('flights.urls')),
+    path('flights', include('flights.urls')),
     path('routes/search', include('routes.urls')),
     path('tickets', include('tickets.urls')),
 
