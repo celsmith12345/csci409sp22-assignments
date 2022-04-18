476181d03231853cde49879003ce08c9ade4d4cd
unknown <cel.smith12345@gmail.com>
2022-04-18 14:06:00 -0400

    Week 11 Assignment

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Flight # Import Flight model
+from airports.models import Airport # Import airport model to get airport id and code
+from .models import Airline
+from .forms import FlightForm
 
 def index(request):
-    return HttpResponse('Hello from flights');
+    # Fetch all flights
+    flights = Flight.objects.all()
+    flight_list = ', '.join([f.origin.airport_code + "->" + f.destination.airport_code for f in flights])
+    return HttpResponse('Listing all flights: ' + flight_list)
+
+def search(request):
+    form = TicketForm(request.POST)
+    if form.is_valid():
+        reservation = Reservation.objects.get(id=form.cleaned_data['confirmation_number'])
+        return render(request, 'tickets/ticket_search.html', {'reservation': reservation})
 
 def flight_search(request, origin, destination):
-    return HttpResponse('Showing flights from: ' + origin + ' to ' + destination)
+    origin = Airport.objects.get(airport_code=origin)
+    destination = Airport.objects.get(airport_code=destination)
+    # Code to select flights from the database
+    flights = Flight.objects.filter(origin_id=origin).filter(destination_id=destination)
+    flight_list = ', '.join([f.origin.airport_code + "->" + f.destination.airport_code + " Airline Code: " +
+                             f.airline.airline_code for f in flights])
+    return HttpResponse('Showing flights: ' + flight_list)
--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Reservation, Ticket
+from airports.models import Airport
+from flights.models import Flight, Airline
+from .forms import TicketForm
 
+
+@login_required()
 def index(request):
-    return HttpResponse('Hello from tickets');
+    # Create an instance of the form class
+    form = TicketForm()
+    return render(request, 'tickets/index.html', {'form': form})
+
+@login_required()
+def search(request):
+    form = TicketForm(request.POST)
+    if form.is_valid():
+        reservation = Reservation.objects.get(id=form.cleaned_data['confirmation_number'])
+        flight = Flight.objects.filter(id=reservation)
+
+
+        return render(request, 'tickets/ticket_search.html', {'reservation': reservation, 'flight': flight})
 
 def ticket_search(request, confirmation_number):
-    return HttpResponse('Showing tickets for: ' + confirmation_number)
+    # Select the singular reservation for the confirmation number
+    # Note: the confirmation_number is the id in the Reservation table
+    reservation = Reservation.objects.get(pk=confirmation_number)
+    return HttpResponse('Number of tickets for confirmation number: ' + str(confirmation_number) + " is " + str(reservation.num_people))
